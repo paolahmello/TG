45317063c261cd45407f09369edb1812ca7fbf6c
Paola Harduim <paolahmello@gmail.com>
2021-04-25 15:25:07 -0300

    separar js

---
+++ static/js/index.js
@@ -0,0 +1,10 @@
+function checkFile(){
+    if($('input[type=file]').val().length == 0){
+        $('button').attr('disabled', true)
+    }
+    else{
+        $('button').attr('disabled', false);
+        var fileName = $('input[type=file]').val().split("\\").pop();
+        $('input[type=file]').siblings(".custom-file-label").addClass("selected").html(fileName);
+    }
+}
